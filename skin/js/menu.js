14053845c27231e81beaed6e2c7fdfe585d8379d
Slimetan <3ktan.95@gmail.com>
2020-01-04 18:32:23 +0700

    fix atlas ex, missing time stone, update sw and wl atlas ex

--- skin/js/menu.js
+++ skin/js/menu.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     // var h = $(window).height();
     $(".l-site").width(w);
 
-    $('.weap').load('gs.html'); 
+    $('.weap').load('wl.html'); 
     $('#select-weap-id').change(function(){ 
         var select_weap = $( "#select-weap-id" ).val(); 
         switch (select_weap){
