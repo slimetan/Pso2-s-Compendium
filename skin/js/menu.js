8d9779e7a322296d05e152a935cd773232433642
Slimetan <3ktan.95@gmail.com>
2020-01-06 13:53:14 +0700

    update all striking weap atlas ex

--- skin/js/menu.js
+++ skin/js/menu.js
@@ -3,44 +3,7 @@ $(document).ready(function(){
     $('.l-nav').load('../../../../skin/layout/menu.html');  
     var w = $(window).width();
     // var h = $(window).height();
-    $(".l-site").width(w);
-
-    // $('.weap').load('gs.html'); 
-    // $('#select-weap-id').change(function(){ 
-    //     var select_weap = $( "#select-weap-id" ).val(); 
-    //     switch (select_weap){
-    //         default:
-    //             $('.weap').load('gs.html'); 
-    //             break;
-    //         case "1": 
-    //             $('.weap').load('gs.html');
-    //             break;
-    //         case "2":
-    //             $('.weap').load('sw.html');
-    //             break; 
-    //         case "3":
-    //             $('.weap').load('wl.html');
-    //             break; 
-    //     } 
-    // });
-    // $('.unit').load('rear.html'); 
-    // $('#select-unit-id').change(function(){ 
-    //     var select_weap = $( "#select-unit-id" ).val();
-    //     switch (select_weap){
-    //         default:
-    //             $('.unit').load('rear.html'); 
-    //             break;
-    //         case "1": 
-    //             $('.unit').load('rear.html');
-    //             break;
-    //         case "2":
-    //             $('.unit').load('arm.html');
-    //             break;   
-    //         case "3":
-    //             $('.unit').load('leg.html');
-    //             break; 
-    //     } 
-    // });
+    $(".l-site").width(w); 
     $('.menu').on('click', function(){
         if ($('.l-site').hasClass('is-open')) {
             $('.menu').removeClass('is-active');
